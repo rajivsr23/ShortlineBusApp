922cc984eb318b0e8acffde360e3fd5f3ebc5242
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-25 21:31:52 -0500

    Wired up the application to write to the Firebase Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,23 @@
 import * as firebase from 'firebase';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
 
-firebase.initializeApp(config);
 
-const database = firebase.database();
+  var config = {
+    apiKey: "AIzaSyBmD3YxYRFyzqsuobRTebY0G4icMrY2Fjo",
+    authDomain: "shortlinebusapp.firebaseapp.com",
+    databaseURL: "https://shortlinebusapp.firebaseio.com",
+    projectId: "shortlinebusapp",
+    storageBucket: "shortlinebusapp.appspot.com",
+    messagingSenderId: "393164937668"
+  };
+   firebase.initializeApp(config);
+ 
+  firebase.database().ref().set({
+name: 'Ricky Raghavan'
+
+  });
+const database=firebase.database();
+
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default };
