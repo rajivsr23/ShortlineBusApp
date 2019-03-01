7abf0d2df705220e7f87c3ee7622ca8484425e21
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-28 22:32:36 -0500

    Created a Destination Component

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import Destination from './Destination';
 const DashboardPage = () => (
   <div>
   <Header/>
+  <Destination/>
     Dashboard page content
   </div>
 );
---
+++ src/components/Destination.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Destination =()=>{
+    return <h2>Destination</h2>
+};
+export default Destination;
