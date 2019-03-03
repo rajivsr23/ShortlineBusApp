c4274a220c509ee0fb520dfeb4acdce13ce8f768
Ricky Raghavan <rajivsr@yahoo.com>
2019-03-02 22:11:49 -0500

    Created a Results Component

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Destination from './Destination';
+import Results from './Results';
 const DashboardPage = () => (
   <div>
   <Header/>
   <Destination/>
     Dashboard page content
+    <Results/>
   </div>
 );
 
---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ const Results=()=>{
+
+  return (
+
+      <div>
+<h2>Results</h2>
+</div>
+  );
+
+};
+
+export default Results;
