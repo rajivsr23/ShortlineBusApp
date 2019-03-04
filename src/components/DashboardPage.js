e95063ccdc2cd47b59873446d87dfa7bc8c0f83d
Ricky Raghavan <rajivsr@yahoo.com>
2019-03-03 22:10:07 -0500

    Created a Search  Component

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Destination from './Destination';
+import Search from './Search';
 import Results from './Results';
 const DashboardPage = () => (
   <div>
   <Header/>
   <Destination/>
     Dashboard page content
+    <Search/>
     <Results/>
   </div>
 );
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ const Search=()=>{
+
+  return (
+
+      <div>
+<h2>Search</h2>
+</div>
+  );
+
+};
+
+export default Search;
